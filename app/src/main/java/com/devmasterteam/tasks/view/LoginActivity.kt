a87631a5fdb395913f76c2f4a19a7baa23e326a0
João Vitor <vitormendes388@gmail.com>
2024-05-05 17:58:15 -0300

    implements get priority from API

--- app/src/main/java/com/devmasterteam/tasks/view/LoginActivity.kt
+++ app/src/main/java/com/devmasterteam/tasks/view/LoginActivity.kt
@@ -31,6 +31,8 @@ class LoginActivity : AppCompatActivity(), View.OnClickListener {
         binding.buttonLogin.setOnClickListener(this)
         binding.textRegister.setOnClickListener(this)
 
+        viewModel.verifyLoggedUser()
+
         // Observadores
         observe()
     }
@@ -50,6 +52,13 @@ class LoginActivity : AppCompatActivity(), View.OnClickListener {
                 finish()
             } else {
                 Toast.makeText(applicationContext, it.message(), Toast.LENGTH_SHORT).show()
+            }
+        }
+
+        viewModel.loggedUser.observe(this){
+            if (it) {
+                startActivity(Intent(applicationContext, MainActivity::class.java))
+                finish()
             }
         }
     }
